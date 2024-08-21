cc5b380dcfffe38f7232f938ccaa4ff4730e38fc
Adolfo <vasquezadolfo482@gmail.com>
2024-08-21 01:50:09 -0400

    conexion a la base

--- config/connection.js
+++ config/connection.js
@@ -1,5 +1,6 @@
 const mysql = require('mysql2/promise');
 
+// Crear la conexión
 const pool = mysql.createPool({
     host: process.env.DB_HOST,
     port: process.env.DB_PORT,
@@ -8,7 +9,7 @@ const pool = mysql.createPool({
     database: process.env.DB_NAME
 });
 
-
+// Verificar la conexión al obtener una conexión del pool
 const validateConnection = async () => {
     try {
         const connection = await pool.getConnection();
